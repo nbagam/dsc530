a6df277cda5657b18fd22d857624bfa652ab42fe
Naveen Bagam <nbagam@gmail.com>
2022-03-27 20:47:10 -0500

    DSC530-week2

---
+++ DSC530-T301-WEEK-2.py
@@ -0,0 +1,41 @@
+#########################################################################################################
+# File name: prepare_eda.py
+# Subject code: DSC 530
+# Week : 2
+# Programming Assignment Week : 2
+# Author: Naveen Bagam
+# Date created: 03/27/2022
+# Reviewer: Shankar Parajulee
+# Description:  This programme performs different operations in python to prepare for the EDA
+#######################################################################################################
+#######################################################################################################
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print("Hello World!")
+
+# Add two numbers together
+print("Addition:2+2 = ", 2 + 2)
+
+# Subtract a number from another number
+print("Subtraction: 10-5 = ", 10 - 5)
+
+# Multiply two numbers
+print("Multiply : 5*5 = ", 5 * 5)
+
+# Divide between two numbers
+print("Divide 10/2 = ", 10 / 2)
+
+# Concatenate two strings together (any words)
+print("Concatenate Strings Naveen and Bagam : ", "Naveen" + "Bagam")
+
+# Create a list of 4 items (can be strings, numbers, both)
+my_list = ["Naveen", "Bagam", 2022, '$8000']
+print("List item: ", my_list)
+
+# Append an item to your list (again, can be a string, number)
+my_list.append("DSC530")
+print(my_list)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+my_tuple = (300,400,600,700,800)
+print(my_tuple)
